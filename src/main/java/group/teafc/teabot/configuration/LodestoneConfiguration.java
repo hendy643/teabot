ef1011e49e64cc8bccd1c74d3ebd3b863b93eb14
Wompus <wompussenekane@hotmail.com>
2022-07-13 22:24:28 +0100

    dep thingy

--- src/main/java/group/teafc/teabot/configuration/LodestoneConfiguration.java
+++ src/main/java/group/teafc/teabot/configuration/LodestoneConfiguration.java
@@ -1,14 +1,14 @@
 package group.teafc.teabot.configuration;
 
-import com.github.wompus.lodestone4j.Lodestone;
+import group.teafc.lodestone.Lodestone;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LodestoneConfiguration {
 
-    @Bean
-    public Lodestone lodestone() {
-        return new Lodestone();
-    }
+  @Bean
+  public Lodestone lodestone() {
+    return Lodestone.getInstance();
+  }
 }
