ebdf7b67ec3fa88cdc9221e430188ddc31315cf2
Wompus <wompussenekane@hotmail.com>
2022-07-13 22:45:46 +0100

    what happened to mongo?

--- src/main/java/group/teafc/teabot/db/entities/GuildEntity.java
+++ src/main/java/group/teafc/teabot/db/entities/GuildEntity.java
@@ -1,37 +1,20 @@
 package group.teafc.teabot.db.entities;
 
 import lombok.*;
-import org.hibernate.Hibernate;
-
-import javax.persistence.*;
-import java.util.Objects;
-
+import org.bson.codecs.pojo.annotations.BsonId;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity(name = "bot_registered_guilds")
+@EqualsAndHashCode
+@Document
 public class GuildEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @BsonId private ObjectId id;
 
-    @Column(unique = true, nullable = false)
-    private Long guildId;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        GuildEntity that = (GuildEntity) o;
-        return id != null && Objects.equals(id, that.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
+  private Long guildId;
 }
--- src/main/java/group/teafc/teabot/db/repositories/GuildRepository.java
+++ src/main/java/group/teafc/teabot/db/repositories/GuildRepository.java
@@ -1,10 +1,11 @@
 package group.teafc.teabot.db.repositories;
 
 import group.teafc.teabot.db.entities.GuildEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GuildRepository extends JpaRepository<GuildEntity, Long> {
-    GuildEntity findByGuildId(Long guildId);
+public interface GuildRepository extends MongoRepository<GuildEntity, ObjectId> {
+    GuildEntity findByGuildId(ObjectId guildId);
 }
